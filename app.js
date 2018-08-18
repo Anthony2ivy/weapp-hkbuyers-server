06d2f63efc38380590531b894174cf6a6dd65a7d
Anthony <729915362@qq.com>
2018-08-18 22:53:45 +0800

    crt

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const bodyParser = require('body-parser');
 const goodRoute =require('./routers/goodRouter');
 const app = express();
 const multer=require("multer");
+const fs = require('fs');
+const https=require('https')
+const privatekey=fs.readFileSync('./bin/www.shitaidaigou.club.key','utf-8');
+const cert=fs.readFileSync('./bin/www.shitaidaigou.club.crt','utf-8');
 
 let storage = multer.diskStorage({
    destination: function (req,file,cd) {
@@ -24,11 +28,16 @@ app.post('/img',upload.single('img'),function (req,res) {
     let imgPath =req.file.path;
     res.json({path:imgPath});
 });
+const server =https.createServer({
+    key:privatekey,
+    cert:cert
+},app);
 
-const server = app.listen(3000, function () {
+
+server.listen(3000, function () {
     let host = server.address().address;
     let port = server.address().port;
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at https://%s:%s', host, port);
 });
 
 
